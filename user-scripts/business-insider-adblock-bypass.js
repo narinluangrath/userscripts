7f97e43fac8fc8c94980095d341e4b0932cd9633
Narin Luangrath <narin.luangrath@gmail.com>
2020-10-09 01:15:43 -0700

    add script metadata

--- user-scripts/business-insider-adblock-bypass.js
+++ user-scripts/business-insider-adblock-bypass.js
@@ -1,3 +1,9 @@
+// ==UserScript==
+// @name Business Insider Adblock Bypass
+// @version 1.0
+// @match https://www.businessinsider.com/*
+// ==/UserScript==
+
 (function () {
   document.getElementsByClassName("tp-modal")[0].remove()
   document.getElementsByClassName("tp-backdrop tp-active")[0].remove()
