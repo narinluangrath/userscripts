28be7f9d4d818ec5de75ab23499e86ddfd430157
Narin Luangrath <narin.luangrath@gmail.com>
2020-12-06 18:23:34 -0800

    add alyssa script

---
+++ user-scripts/alyssa.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name Name
+// @version 1.0
+// @match https://www.bees.com/*
+// ==/UserScript==
+
+(function () {
+  alert("Alyssa is the greatest! Click OK if you agree!!!"
+  )
+})()
