54e8816bcbad7dab859f0537bb669cadbfcdcc19
Narin Luangrath <narin.luangrath@gmail.com>
2020-12-06 18:28:05 -0800

    update alyssa script

--- user-scripts/alyssa.js
+++ user-scripts/alyssa.js
@@ -1,10 +1,9 @@
 // ==UserScript==
-// @name Name
+// @name Alyssa
 // @version 1.0
-// @match https://www.bees.com/*
+// @match https://www.amazon.com/*
 // ==/UserScript==
 
 (function () {
-  alert("Alyssa is the greatest! Click OK if you agree!!!"
-  )
+  alert("Alyssa is the greatest! Click OK if you agree!!!")
 })()
