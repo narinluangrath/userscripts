0d46b751bd799e4b84f7dc3dc78f57c8fdf4b7d9
Narin Luangrath <narin.luangrath@gmail.com>
2020-12-26 01:23:02 -0800

    chore: delete testing scripts

--- user-scripts/alyssa.js
+++
@@ -1,9 +0,0 @@
-// ==UserScript==
-// @name Alyssa
-// @version 1.0
-// @match https://www.bees.com/*
-// ==/UserScript==
-
-(function () {
-  alert("Alyssa is the greatest! Click OK if you agree!!!")
-})()
