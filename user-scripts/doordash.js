926fdcc58f1558b5fddc554cf66821587605aabf
Narin Luangrath <narin.luangrath@gmail.com>
2020-12-28 20:02:09 -0800

    add doordash reminder script

---
+++ user-scripts/doordash.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name Doordash
+// @version 1.0
+// @match https://www.doordash.com/consumer/checkout/*
+// ==/UserScript==
+
+(function () {
+  alert("Don't forget to check the location!")
+})()
